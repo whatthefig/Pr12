89701fcb445ef19e944f4b8d9fe6908e1394946f
Царицын Андрей Андреевич <aa_tsaritsyn@bbgroup.local>
2019-12-16 03:32:34 +0300

    исправления

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,19 @@
 const routesUsers = require('express').Router();
-
 const fs = require('fs');
+const path = require('path');
 
-fs.readFile('./data/users.json', { encoding: 'utf8' }, (err, data) => {
-  const users = JSON.parse(data);
-  routesUsers.get('/users', (req, res) => {
+const filepath = path.join('./data', 'users.json');
+
+routesUsers.get('/users', (req, res) => {
+  fs.readFile(filepath, { encoding: 'utf8' }, (err, data) => {
+    const users = JSON.parse(data);
     res.send(users);
   });
-  routesUsers.get('/users', (req, res) => {
-    res.send(users);
-  });
-  routesUsers.get('/users/:id', (req, res) => {
+});
+
+routesUsers.get('/users/:id', (req, res) => {
+  fs.readFile(filepath, { encoding: 'utf8' }, (err, data) => {
+    const users = JSON.parse(data);
     const { id } = req.params;
     const user = users.find(user => id === user._id);
     if (user) {
